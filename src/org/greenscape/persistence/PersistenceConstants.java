612985913056e67e9c5adff0e7bc066728be5986
smsajid <smsajid786@gmail.com>
2014-07-04 23:42:59 +0530

    add factory ds constant

--- src/org/greenscape/persistence/PersistenceConstants.java
+++ src/org/greenscape/persistence/PersistenceConstants.java
@@ -3,4 +3,5 @@ package org.greenscape.persistence;
 public interface PersistenceConstants {
 	String CONFIG_NAME = "org.greenscape.persistence";
 	String PROP_MODEL_AUTOCREATE = "persistence.model.autocreate";
+	String ModelRegistryEntry_FACTORY_DS = "org.greenscape.persistence.ModelRegistryEntry.factory";
 }
