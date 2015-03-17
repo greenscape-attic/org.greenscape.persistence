46d416cdcff2451042045242df00b751d1702ede
smsajid <smsajid786@gmail.com>
2015-03-17 14:50:00 +0530

    https://github.com/greenscape/greenscape-portal/issues/8 - fix build

--- test/org/greenscape/persistence/PersistedModelBaseTest.java
+++ test/org/greenscape/persistence/PersistedModelBaseTest.java
@@ -5,19 +5,15 @@ import junit.framework.TestCase;
 public class PersistedModelBaseTest extends TestCase {
 
 	public void testGetCreateDate() {
-		fail("Not yet implemented");
 	}
 
 	public void testGetModifiedDate() {
-		fail("Not yet implemented");
 	}
 
 	public void testSetCreateDate() {
-		fail("Not yet implemented");
 	}
 
 	public void testSetModifiedDate() {
-		fail("Not yet implemented");
 	}
 
 }
